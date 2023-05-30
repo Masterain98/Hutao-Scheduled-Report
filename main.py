d14eb22377675b552aa38c00bd25a3057c9dc16b
Masterain <i@irain.in>
2023-05-30 02:59:01 -0700

    Update main.py

--- main.py
+++ main.py
@@ -45,39 +45,13 @@ def user_per_schedule_bar():
 
 
 def uid_layout():
-    sql = r"SELECT Uid, UploadTime, Uploader FROM records RIGHT JOIN spiral_abysses ON " \
-          r"records.PrimaryId=spiral_abysses.RecordId"
-    sql_result = db.fetch_all(sql)
-
-    # Option 1
-    # This is a straight forward option, good for single chart but too hard for extensions.
-    """
-    china_uid_list = [{"UID": int(item[0][:3]), "Time": datetime.fromtimestamp(item[1]), "Uploader": item[2]} for item
-                      in list(sql_result) if item[0].startswith(("1", "2"))]
-    fig = px.scatter(data_frame=china_uid_list, y="Time", x="UID", color="Uploader", color_discrete_map=uploader_color,
-                     labels={
-                         "UID": "Starting Number of UID",
-                         "Time": "Datetime"
-                     })
-    
-    # For UID Group charts              
-    uid_group_count = {}
-    for record in china_uid_list:
-        this_uid_group = record["UID"]
-        if this_uid_group not in uid_group_count.keys():
-            uid_group_count[this_uid_group] = 1
-        else:
-            uid_group_count[this_uid_group] += 1
-
-    fig = go.Figure()
-    fig.add_bar(x=list(uid_group_count.keys()), y=list(uid_group_count.values()))
-    fig.update_layout()
-    fig.show()
-    """
-
     # Option 2
     # Convert SQL result into a dataframe, add charts into trace.
     # Plotly Graph Object (go) is a basic library of Plotly Express (px)
+
+    sql = r"SELECT Uid, UploadTime, Uploader FROM records RIGHT JOIN spiral_abysses ON " \
+          r"records.PrimaryId=spiral_abysses.RecordId"
+    sql_result = db.fetch_all(sql)
 
     df = pd.DataFrame(list(sql_result))
     df.rename(columns={0: "UID", 1: "Time", 2: "Uploader"}, inplace=True)
@@ -107,6 +81,7 @@ def uid_layout():
                     x=v[v.UID.astype(str).str.startswith(uid)]["UID"],
                     y=v[v.UID.astype(str).str.startswith(uid)]["Time"],
                     name=k,
+                    legendgroup=k,
                     mode="markers",
                     marker=dict(color=v[v.UID.astype(str).str.startswith(uid)]["Uploader"].apply(
                         lambda x: uploader_color[x]))
@@ -115,8 +90,6 @@ def uid_layout():
 
     # Add dropdowns
     button_layer_1_height = 1.08
-    fig.update_layout(showlegend=False, title="Uploader UID Information by Time",
-                      title_x=0.5)
     fig.update_layout(
         updatemenus=[
             dict(
@@ -129,8 +102,7 @@ def uid_layout():
                                            True, True, True,
                                            True, True, True
                                            ]},
-                              {"showlegend": False},
-                              dict(text="Uploader UID Information by Time (All Regions)")],
+                              {"title": "Uploader UID Information by Time (All Regions)"}],
                         label="All",
                         method="update"
                     ),
@@ -142,8 +114,7 @@ def uid_layout():
                                            False, False, False,
                                            False, False, False
                                            ]},
-                              {"showlegend": True},
-                              dict(text="Uploader UID Information by Time (Mainland China)")],
+                              {"title": "Uploader UID Information by Time (Mainland China)"}],
                         label="China",
                         method="update"
                     ),
@@ -155,8 +126,7 @@ def uid_layout():
                                            False, False, False,
                                            False, False, False
                                            ]},
-                              {"showlegend": True},
-                              {"title_text": "Uploader UID Information by Time (bilibili @ Mainland China)"}],
+                              {"title": "Uploader UID Information by Time (bilibili @ Mainland China)"}],
                         label="bilibili",
                         method="update"
                     ),
@@ -168,8 +138,7 @@ def uid_layout():
                                            False, False, False,
                                            False, False, False
                                            ]},
-                              {"showlegend": True},
-                              {"title_text": "Uploader UID Information by Time (America)"}],
+                              {"title": "Uploader UID Information by Time (America)"}],
                         label="America",
                         method="update"
                     ),
@@ -181,8 +150,7 @@ def uid_layout():
                                            False, False, False,
                                            False, False, False
                                            ]},
-                              {"showlegend": True},
-                              {"title_text": "Uploader UID Information by Time (Europe)"}],
+                              {"title": "Uploader UID Information by Time (Europe)"}],
                         label="EU",
                         method="update"
                     ),
@@ -194,8 +162,7 @@ def uid_layout():
                                            True, True, True,
                                            False, False, False
                                            ]},
-                              {"showlegend": True},
-                              {"title_text": "Uploader UID Information by Time (Asia)"}],
+                              {"title": "Uploader UID Information by Time (Asia)"}],
                         label="Asia",
                         method="update"
                     ),
@@ -207,12 +174,12 @@ def uid_layout():
                                            False, False, False,
                                            True, True, True
                                            ]},
-                              {"showlegend": True},
-                              {"title_text": "Uploader UID Information by Time (Taiwan/Hong Kong/Macau)"}],
+                              {"title": "Uploader UID Information by Time (Taiwan/Hong Kong/Macau)"}],
                         label="TW/HK/MO",
                         method="update"
                     ),
-                ]),
+                ]
+                ),
                 direction="down",
                 pad={"r": 10, "t": 10},
                 showactive=True,
@@ -220,14 +187,51 @@ def uid_layout():
                 xanchor="left",
                 y=button_layer_1_height,
                 yanchor="top"
-            ),
+            )
         ]
     )
-
+    fig.update_layout(showlegend=True, title_text="Uploader UID Information by Time",
+                      title_x=0.5)
     pio.write_html(fig, file="output/uploader_info.html", auto_open=True, include_plotlyjs="cdn",
                    include_mathjax="cdn")
+
+
+def uid_layout_old():
+    # Option 1
+    # This is a straight forward option, good for single chart but too hard for extensions.
+    uploader_color = {
+        "Snap Hutao": "rgb(239,85,59)",
+        "Snap Hutao Bookmark": "rgb(0,204,150)",
+        "miao-plugin": "rgb(99,110,250)"
+    }
+    sql = r"SELECT Uid, UploadTime, Uploader FROM records RIGHT JOIN spiral_abysses ON " \
+          r"records.PrimaryId=spiral_abysses.RecordId"
+    sql_result = db.fetch_all(sql)
+
+    # Option 1
+    # This is a straight forward option, good for single chart but too hard for extensions.
+
+    china_uid_list = [{"UID": int(item[0][:3]), "Time": datetime.fromtimestamp(item[1]), "Uploader": item[2]} for item
+                      in list(sql_result) if item[0]]
+    fig = px.scatter(data_frame=china_uid_list, y="Time", x="UID", color="Uploader", color_discrete_map=uploader_color,
+                     labels={
+                         "UID": "Starting Number of UID",
+                         "Time": "Datetime"
+                     })
+
+    # For UID Group charts
+    uid_group_count = {}
+    for record in china_uid_list:
+        this_uid_group = record["UID"]
+        if this_uid_group not in uid_group_count.keys():
+            uid_group_count[this_uid_group] = 1
+        else:
+            uid_group_count[this_uid_group] += 1
+
+    fig.show()
 
 
 if __name__ == "__main__":
     user_per_schedule_bar()
     uid_layout()
+    #uid_layout_old()
